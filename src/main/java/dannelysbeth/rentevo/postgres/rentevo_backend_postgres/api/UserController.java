c5c7dcaaae6c9500a4b76e73168a91ec8624a6ac
danifoj315 <danifoj315@student.polsl.pl>
2023-09-28 23:40:54 +0200

    added user by id

--- src/main/java/dannelysbeth/rentevo/postgres/rentevo_backend_postgres/api/UserController.java
+++ src/main/java/dannelysbeth/rentevo/postgres/rentevo_backend_postgres/api/UserController.java
@@ -1,7 +1,20 @@
 package dannelysbeth.rentevo.postgres.rentevo_backend_postgres.api;
 
+import dannelysbeth.rentevo.postgres.rentevo_backend_postgres.model.User;
+import dannelysbeth.rentevo.postgres.rentevo_backend_postgres.service.definition.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
+
+    UserService userService;
+
+    @GetMapping("/{id}")
+    User getUserById(@PathVariable String id) {
+        return userService.getUserById(Long.getLong(id));
+    }
 }
