8d2429ac615d89bb0ec01dfc43f2c52881b44f3a
Dannelysbeth <danielaf@onet.pl>
2024-03-09 23:28:20 +0100

    updated Product Repository

--- src/main/java/dannelysbeth/rentevo/postgres/rentevo_backend_postgres/repository/ProductRepository.java
+++ src/main/java/dannelysbeth/rentevo/postgres/rentevo_backend_postgres/repository/ProductRepository.java
@@ -4,6 +4,10 @@ import dannelysbeth.rentevo.postgres.rentevo_backend_postgres.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Set<Product> getProductsByProductCategory_Id(Long categoryId);
+    Set<Product> getProductsByName(String productName);
 }
