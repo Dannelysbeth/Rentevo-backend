5d838ec4397300c07fc1dad007d6e240c1129662
Dannelysbeth <danielaf@onet.pl>
2024-03-17 23:03:23 +0100

    updated user controller

--- src/main/java/dannelysbeth/rentevo/postgres/rentevo_backend_postgres/api/UserController.java
+++ src/main/java/dannelysbeth/rentevo/postgres/rentevo_backend_postgres/api/UserController.java
@@ -2,10 +2,8 @@ package dannelysbeth.rentevo.postgres.rentevo_backend_postgres.api;
 
 import dannelysbeth.rentevo.postgres.rentevo_backend_postgres.model.User;
 import dannelysbeth.rentevo.postgres.rentevo_backend_postgres.service.definition.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -14,7 +12,9 @@ public class UserController {
     UserService userService;
 
     @GetMapping("/{id}")
-    User getUserById(@PathVariable String id) {
-        return userService.getUserById(Long.getLong(id));
+    ResponseEntity<User> getUserById(@PathVariable String id) {
+
+        return ResponseEntity.ok()
+                .body(userService.getUserById(Long.getLong(id)));
     }
 }
