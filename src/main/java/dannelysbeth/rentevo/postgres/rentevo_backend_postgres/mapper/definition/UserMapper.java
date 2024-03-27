e8a42382953e3a7df72a51903b5804bba0a9dd8c
Dannelysbeth <danielaf@onet.pl>
2024-03-27 23:54:05 +0100

    added implementation of UserMapper

--- src/main/java/dannelysbeth/rentevo/postgres/rentevo_backend_postgres/mapper/definition/UserMapper.java
+++ src/main/java/dannelysbeth/rentevo/postgres/rentevo_backend_postgres/mapper/definition/UserMapper.java
@@ -1,5 +1,6 @@
 package dannelysbeth.rentevo.postgres.rentevo_backend_postgres.mapper.definition;
 
+import dannelysbeth.rentevo.postgres.rentevo_backend_postgres.enums.Role;
 import dannelysbeth.rentevo.postgres.rentevo_backend_postgres.model.DTO.request.UserRequest;
 import dannelysbeth.rentevo.postgres.rentevo_backend_postgres.model.DTO.response.UserResponse;
 import dannelysbeth.rentevo.postgres.rentevo_backend_postgres.model.User;
@@ -8,7 +9,7 @@ import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface UserMapper {
-    User tranformRequestToUser(UserRequest userRequest);
+    User tranformRequestToUser(UserRequest request, String encodedPassword, Role role);
 
     UserResponse transformUserToResponse(User user);
 }
---
+++ src/main/java/dannelysbeth/rentevo/postgres/rentevo_backend_postgres/mapper/implementation/UserMapperImpl.java
@@ -0,0 +1,31 @@
+package dannelysbeth.rentevo.postgres.rentevo_backend_postgres.mapper.implementation;
+
+import dannelysbeth.rentevo.postgres.rentevo_backend_postgres.enums.Role;
+import dannelysbeth.rentevo.postgres.rentevo_backend_postgres.mapper.definition.UserMapper;
+import dannelysbeth.rentevo.postgres.rentevo_backend_postgres.model.DTO.request.UserRequest;
+import dannelysbeth.rentevo.postgres.rentevo_backend_postgres.model.DTO.response.UserResponse;
+import dannelysbeth.rentevo.postgres.rentevo_backend_postgres.model.User;
+
+public class UserMapperImpl implements UserMapper {
+    @Override
+    public User tranformRequestToUser(UserRequest request, String encodedPassword, Role role) {
+        return User.builder()
+                .role(role)
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .username(request.getUsername())
+                .password(encodedPassword)
+                .build();
+    }
+
+    @Override
+    public UserResponse transformUserToResponse(User user) {
+        return UserResponse.builder()
+                .email(user.getEmail())
+                .username(user.getUsername())
+                .firstname(user.getFirstname())
+                .lastname(user.getLastname())
+                .build();
+    }
+}
